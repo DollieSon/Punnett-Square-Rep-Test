b91d1c5d9645ad16af936d5aa3672db313ccf40f
DollieSon <62559681+DollieSon@users.noreply.github.com>
2021-05-31 21:18:40 +0800

    Add files via upload

---
+++ Repition Test.py
@@ -0,0 +1,20 @@
+Loop=int(input("Insert Input:"))
+word=[]
+Wword=""
+seq=""
+rep = False
+print("input Seq:\n")
+while Loop > 0 :
+    seq=str(input())
+    for x in word:
+        if x == seq:
+            print("Already Exists")
+            rep = True
+    if rep == False:
+        word.append(str(seq))
+    else:
+        rep = False
+    Loop -=1
+for x in word:
+    Wword+=f"{x}"
+print(word)
